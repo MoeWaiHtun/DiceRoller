114af41abf613e3181abbfe19b825f7296dc4689
Moe Wai Htun <moewaihtun74@gmail.com>
2024-05-11 19:01:21 +0630

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+import com.android.build.api.dsl.BuildFeatures
+
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "it.moe.dice"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "it.moe.dice"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures{
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
